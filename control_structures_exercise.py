2b24339808686ae358354a79fcffa6fd28ce7c27
Siyamthanda <sityindjhb024@student.wethinkcode.co.za>
2024-09-18 15:50:37 +0200

    testing

--- control_structures_exercise.py
+++ control_structures_exercise.py
@@ -91,7 +91,7 @@ multiplier_str = "Enter a number as the multiplier: "
 for number_x in range(1, 11):
     for number_y in range(1):
         # Add code to print the multiplication table
-        print()
+        print("hi")
         
         pass
     pass
